c1c51f10e20385921ed0ee43aa9b177ecebe7488
Jim Vargas <jimball1@hotmail.com>
2017-08-22 23:02:11 -0700

    initial commit, appears to do everything it needs to; no bonuses

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -23,5 +24,33 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        if (searchType.equalsIgnoreCase("all") && (searchTerm.equalsIgnoreCase(""))){
+            ArrayList<HashMap<String, String>> relevantJobs = JobData.findAll();
+            model.addAttribute("relevantJobs", relevantJobs);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("dumdum", "dumdum");
+        } else if (searchType.equalsIgnoreCase("all") && !(searchTerm.equalsIgnoreCase(""))){
+            ArrayList<HashMap<String, String>> allJobs = JobData.findAll();
+            ArrayList<HashMap<String, String>> relevantJobs = new ArrayList<>();
 
+            for (int i = 0; i < (allJobs.size()); i++) {
+                for (Map.Entry<String, String> item : allJobs.get(i).entrySet()) {
+                    if (item.getValue().toLowerCase().contains(searchTerm.toLowerCase())){
+                        relevantJobs.add(allJobs.get(i));
+                        break;
+                    }
+                }
+            }
+
+            model.addAttribute("relevantJobs", relevantJobs);
+            model.addAttribute("columns", ListController.columnChoices);
+        } else {
+            ArrayList<HashMap<String, String>> relevantJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("relevantJobs", relevantJobs);
+            model.addAttribute("columns", ListController.columnChoices);
+        }
+        return "search";
+    }
 }
